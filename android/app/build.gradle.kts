f0dacc0a5ccdde1f40370ee44224de51f2bdeb63
raafi <raafistudent@gmail.com>
2025-12-23 21:31:57 +0700

    Fix build.gradle: Enable coreLibraryDesugaring and MultiDex

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -11,6 +10,9 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        // --- PERBAIKAN 1: Mengaktifkan Desugaring ---
+        coreLibraryDesugaringEnabled true
+        
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -20,20 +22,18 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.jaganetra"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        
+        // --- PERBAIKAN 2: Mengaktifkan MultiDex (Penting) ---
+        multiDexEnabled true 
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -41,4 +41,10 @@ android {
 
 flutter {
     source = "../.."
+}
+
+// --- PERBAIKAN 3: Menambahkan Library Desugar ---
+dependencies {
+    // Library ini wajib ada untuk flutter_local_notifications di Android lama/baru
+    coreLibraryDesugaring 'com.android.tools:desugar_jdk_libs:2.0.4'
 }
